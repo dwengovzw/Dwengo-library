663977382dca92a03895ff32a69d87b056f83db9
juanpi <juanpi@Ueberland-linux.(none)>
2012-11-29 11:24:11 +0100

    change GPl to LGP

--- lib/dwengoNotes.h
+++ lib/dwengoNotes.h
@@ -1,3 +1,28 @@
+/**
+ * dwengoNotes.h
+ *
+ * Defines for muscial notes.
+ *
+ * Version: 1.0.0
+ * Date:  Monday, November 26 2012
+ *
+ * Copyright (C) 2012 - Juan Pablo Carbajal
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #define MIN_OCTAVE 2
 
 rom const unsigned int n_C[7] = {45455,23077,11450,5725,2868,1434,717};
